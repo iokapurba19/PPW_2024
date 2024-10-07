1822d8b9dc741c775fb91b22b4a505831856dbf5
iokapurba19 <153580385+iokapurba19@users.noreply.github.com>
2024-10-07 09:06:59 +0700

    Login -> Home dashboard

--- app/Http/Controllers/AuthManager.php
+++ app/Http/Controllers/AuthManager.php
@@ -23,9 +23,11 @@ class AuthManager extends Controller
         ]);
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
-            return redirect(route('home'));
+            // return redirect(route('home'));
+            return view('home');
         }
-        return redirect(route('home'))->with("error","Login details are not valid");
+        return view('home');
+        // return redirect(route('home'))->with("error","Login details are not valid");
         
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
