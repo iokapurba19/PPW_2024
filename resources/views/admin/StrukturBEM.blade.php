d0214934561645824124ef7fb82b8194f3ea799e
iokapurba19 <153580385+iokapurba19@users.noreply.github.com>
2024-10-11 15:35:35 +0700

    update

--- resources/views/admin/StrukturBEM.blade.php
+++ resources/views/admin/StrukturBEM.blade.php
@@ -6,56 +6,57 @@
     <title>Simple Table</title>
     @vite('resources/css/app.css')
 </head>
+
 <body class="bg-gray-100">
-
-<div class="w-20 h-screen bg-gray-800 p-5 fixed flex flex-col items-center space-y-10">
-        <!-- Back button -->
-        <button class="text-white text-2xl">&larr;</button>
-
-        <!-- Menu Items -->
-        <ul class="space-y-8">
-            <!-- Active Dashboard -->
-            <li class="flex flex-col items-center">
-                <a href="/" class="bg-yellow-300 p-3 rounded-full">
-                    <img src="https://img.icons8.com/ios-filled/50/000000/dashboard.png" alt="Dashboard Icon" class="h-6 w-6"/>
-                </a>
-                <span class="text-white text-xs mt-2">Dashboard</span>
-            </li>
-
-            <!-- Dropdown Menu: Tour Package -->
-            <li class="relative flex flex-col items-center">
-                <button onclick="toggleDropdown()" class="bg-gray-100 p-3 rounded-full focus:outline-none">
-                    <img src="https://img.icons8.com/ios-filled/50/000000/suitcase.png" alt="Tour Package Icon" class="h-6 w-6"/>
-                </button>
-                <span class="text-gray-300 text-xs mt-2">Content</span>
-
-                <!-- Dropdown items (di sebelah kanan) -->
-                <ul id="dropdown-menu" class="hidden absolute left-full top-0 bg-gray-700 text-white mt-2 space-y-2 rounded-lg w-32 text-center">
-                    <li class="relative group">
-                     <a href="{{ route('creatorcontent') }}" class="block py-2 hover:bg-gray-600">Departemen</a>    
-                 </li>
-                    <li><a href="#" class="block py-2 hover:bg-gray-600">UKM</a></li>
-                </ul>
-
-            <!-- User -->
-            <li class="flex flex-col items-center">
-                <a href="tours" class="bg-gray-100 p-3 rounded-full">
-                    <img src="https://img.icons8.com/ios-filled/50/000000/walking-tour.png" alt="Tours Icon" class="h-6 w-6"/>
-                </a>
-                <span class="text-gray-300 text-xs mt-2">USER</span>
-            </li>
-            
-            <li class="flex flex-col items-center">
-                <a href="strukturbem" class="bg-gray-100 p-3 rounded-full">
-                    <img src="" alt="Tours Icon" class="h-6 w-6"/>
-                </a>
-                <span class="text-gray-300 text-xs mt-2">Anggota BEM</span>
-            </li>
-        </ul>
+<div class="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[200px] overflow-y-auto text-center bg-gray-900">
+      <div class="text-gray-100 text-xl">
+        <div class="p-2.5 mt-1 flex items-center">
+          <i class="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
+          <h1 class="font-bold text-gray-200 text-[15px] ml-3">Admin BEM</h1>
+          <i
+            class="bi bi-x cursor-pointer ml-28 lg:hidden"
+            onclick="openSidebar()"
+          ></i>
+        </div>
+        <div class="my-2 bg-gray-600 h-[1px]"></div>
+      </div>
+      <div
+        class="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white"
+      >
+        <i class="bi bi-search text-sm"></i>
+        <input
+          type="text"
+          placeholder="Search"
+          class="text-[15px] ml-4 w-full bg-transparent focus:outline-none"
+        />
+      </div>
+      <div
+        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+      >
+        <i class="bi bi-house-door-fill"></i>
+        <span class="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
+      </div>
+      <div
+        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+      >
+        <i class="bi bi-bookmark-fill"></i>
+        <span class="text-[15px] ml-4 text-gray-200 font-bold">Departemen</span>
+      </div>
+      <!-- <div class="my-4 bg-gray-600 h-[1px]"></div> -->
+      <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+        <i class="bi bi-chat-left-text-fill"></i>
+        <div class="flex justify-between w-full items-center">
+          <a href="strukturbem"  class="text-[15px] ml-4 text-gray-200 font-bold">Keanggotaan</a>
+        </div>
+      </div>
+      <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+        <i class="bi bi-box-arrow-in-right"></i>
+        <span class="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
+      </div>
     </div>
 
     <div class="container mx-auto p-4">
-        <h1 class="text-2xl font-bold mb-4 ml-20">Struktur Bem Table </h1>
+        <!-- <h1 class="text-2xl font-bold mb-4 ml-20">Struktur Bem Table </h1> -->
 
         <!-- Button Tambah Data dan Tabel dalam satu kontainer -->
         <div class="max-w-4xl mx-auto ml-50">
