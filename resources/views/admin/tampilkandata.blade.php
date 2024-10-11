d65bf9c40f989afe361718874019774276afa534
iokapurba19 <153580385+iokapurba19@users.noreply.github.com>
2024-10-11 15:50:44 +0700

    routing for admin sidebar

--- resources/views/admin/tampilkandata.blade.php
+++ resources/views/admin/tampilkandata.blade.php
@@ -1,3 +1,7 @@
+@extends('admin.sidebar.app')
+
+@section('content')
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,53 +11,6 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
-
-<div class="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[200px] overflow-y-auto text-center bg-gray-900">
-      <div class="text-gray-100 text-xl">
-        <div class="p-2.5 mt-1 flex items-center">
-          <i class="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
-          <h1 class="font-bold text-gray-200 text-[15px] ml-3">Admin BEM</h1>
-          <i
-            class="bi bi-x cursor-pointer ml-28 lg:hidden"
-            onclick="openSidebar()"
-          ></i>
-        </div>
-        <div class="my-2 bg-gray-600 h-[1px]"></div>
-      </div>
-      <div
-        class="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white"
-      >
-        <i class="bi bi-search text-sm"></i>
-        <input
-          type="text"
-          placeholder="Search"
-          class="text-[15px] ml-4 w-full bg-transparent focus:outline-none"
-        />
-      </div>
-      <div
-        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-      >
-        <i class="bi bi-house-door-fill"></i>
-        <span class="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
-      </div>
-      <div
-        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-      >
-        <i class="bi bi-bookmark-fill"></i>
-        <span class="text-[15px] ml-4 text-gray-200 font-bold">Departemen</span>
-      </div>
-      <!-- <div class="my-4 bg-gray-600 h-[1px]"></div> -->
-      <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-        <i class="bi bi-chat-left-text-fill"></i>
-        <div class="flex justify-between w-full items-center">
-          <a href="strukturbem"  class="text-[15px] ml-4 text-gray-200 font-bold">Keanggotaan</a>
-        </div>
-      </div>
-      <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-        <i class="bi bi-box-arrow-in-right"></i>
-        <span class="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
-      </div>
-    </div>
 
     <div class="container mx-auto p-6">
         <h1 class="text-3xl font-bold mb-6 text-center">Simple Table Example</h1>
@@ -181,3 +138,5 @@
 
 </body>
 </html>
+
+@endsection
