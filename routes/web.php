16882566058548d0b0efcb2701cbd6a01464b792
iokapurba19 <153580385+iokapurba19@users.noreply.github.com>
2024-11-15 22:25:45 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::get('/register', [AuthManager::class, 'register'])->name('register');
 Route::post('/register', [AuthManager::class, 'registerPost'])->name('register.post'); 
 Route::get('/login', [AuthManager::class, 'login'])->name('login');
 Route::post('/login', [AuthManager::class, 'loginPost'])->name('login.post'); 
+Route::post('/login', [AuthManager::class, 'loginPost'])->name('login.post'); 
 
 Route::get('/about', function () {
     return view('aboutus');
